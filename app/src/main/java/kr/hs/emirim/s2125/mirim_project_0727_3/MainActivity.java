3973c65c7e33dfdba4feeba66448d23d87e795b9
seungchae1 <s2125@e-mirim.hs.kr>
2022-07-27 15:11:11 +0900

    Activity 이동

--- app/src/main/java/kr/hs/emirim/s2125/mirim_project_0727_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2125/mirim_project_0727_3/MainActivity.java
@@ -2,13 +2,36 @@ package kr.hs.emirim.s2125.mirim_project_0727_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
-
+    RadioGroup rg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnStart = findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(btnStartListener);
+        rg = findViewById(R.id.rg);
     }
+    View.OnClickListener btnStartListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Class c=null;
+            switch (rg.getCheckedRadioButtonId()){
+                case R.id.rd_second:
+                    c=SecondActivity.class;
+                    break;
+                case R.id.rd_third:
+                    c=ThirdActivity.class;
+                    break;
+            }
+            Intent intent = new Intent(getApplicationContext(),c);
+            startActivity(intent);
+        }
+    };
 }
--- app/src/main/java/kr/hs/emirim/s2125/mirim_project_0727_3/ThirdActivity.java
+++ app/src/main/java/kr/hs/emirim/s2125/mirim_project_0727_3/ThirdActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SecondActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_third);
         Button btnPrev = findViewById(R.id.btn_prev);
         btnPrev.setOnClickListener(btnPrevListener);
     }
