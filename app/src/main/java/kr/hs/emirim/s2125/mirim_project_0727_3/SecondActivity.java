b62da761b403bc8eb4bd3ae53292d957403f181e
seungchae1 <s2125@e-mirim.hs.kr>
2022-07-27 14:19:51 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/s2125/mirim_project_0727_3/SecondActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2125.mirim_project_0727_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
